24f606efd071769a49272b5de24bd72e1b75a135
Rajdeep <160480035+rajdeep-314@users.noreply.github.com>
2024-07-14 18:58:32 +0530

    Create ostream.py

---
+++ ostream.py
@@ -0,0 +1,48 @@
+import atexit as _atexit
+
+# This one should implement buffers as well
+class ostream:
+    def __init__(self, has_buffer=True):
+        if has_buffer:
+            self._buffer = ''
+        else:
+            self._buffer = None
+
+    def __lshift__(self, other):
+        if type(other) == ostream_manipulator:
+            other(self)
+            return self
+
+        if self._buffer is None:
+            print(other, end = '')
+        else:
+            self._buffer += str(other)
+        return self
+
+    def flush(self):
+        if not (self._buffer is None):
+            print(self._buffer, end = '')
+            self._buffer = ''
+
+
+class ostream_manipulator:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        self.func(*args)
+
+
+def _endl_function(ostream_obj):
+    ostream_obj << '\n'
+    ostream_obj.flush()
+    
+
+cout = ostream()
+cerr = ostream(has_buffer=False)
+endl = ostream_manipulator(_endl_function)
+
+def _exit_stream_flushing():
+    cout.flush()
+
+_atexit.register(_exit_stream_flushing)
